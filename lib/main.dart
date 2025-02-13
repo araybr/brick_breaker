40792e5858b7a6de3c66f6cd69f9053b24e0c694
abritor <araybrito2005@gmail.com>
2025-02-13 16:52:33 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'src/brick_breaker.dart'; // Add this import
+
+void main() {
+  final game = BrickBreaker(); // Modify this line
+  runApp(GameWidget(game: game));
+}
